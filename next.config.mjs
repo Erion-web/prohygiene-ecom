af0512bb3f14c7d275ab254267bf22ccee328a13
eriDev1 <eriDev1@users.noreply.github.com>
2026-08-17 21:37:40 +0200

    updated next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
+
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
+        protocol: "https",
+        hostname: "*.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
+
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000'],
+      allowedOrigins: ["localhost:3000"],
     },
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
